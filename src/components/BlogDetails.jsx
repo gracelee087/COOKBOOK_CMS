64ef6526556d50aff3a7f9a9e4bbb47604a21a54
Sohee Lee <gracelee.087@gmail.com>
2023-06-08 15:30:04 +0200

    separation of RichTextBody

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -22,7 +22,9 @@ export default function BlogDetails() {
   return (
     <Container>
       <Row>
-        <Col>
+
+      {/* food image */}
+        <Col >
           {blog ? (
             <div>
               <p>{blog.author}</p>
@@ -32,11 +34,93 @@ export default function BlogDetails() {
             "Loading...."
           )}
         </Col>
+
+{/* BLOG TITLE */}
         <Col>
-          {blog && blog.body && (
-            <div>{documentToReactComponents(blog.body)}</div>
-          )}
-        </Col>
+  {blog && blog.title && (
+    <h1>{blog.title}</h1>
+  )}
+
+
+{/* H1 Element */}
+<Col>
+  {blog && blog.body && (
+    <div>
+      {documentToReactComponents(blog.body).filter((component) => {
+        // H1 요소만 필터링
+        return component.type === "h1";
+      })}
+    </div>
+  )}
+</Col>
+
+{/* Normal text */}
+<Col>
+  {blog && blog.body && (
+    <div>
+      {blog.body.content.map((node, index) => {
+        if (node.nodeType === "paragraph") {
+          return (
+            <p key={index}>
+              {node.content.map((textNode, textIndex) => {
+                return textNode.value;
+              })}
+            </p>
+          );
+        }
+        return null;
+      })}
+    </div>
+  )}
+</Col>
+
+
+
+{/* table */}
+<Col style={{ border: "1px solid black", padding: "5px" }}>
+  {blog && blog.body && (
+    <div>
+      {blog.body.content.map((node, index) => {
+        if (node.nodeType === "table") {
+          return (
+            <table key={index} style={{ borderCollapse: "collapse", width: "100%" }}>
+              <tbody>
+                {node.content.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {row.content.map((cell, cellIndex) => (
+                      <td key={cellIndex} style={{ border: "1px solid black", padding: "5px" }}>
+                        {documentToReactComponents({
+                          nodeType: "document",
+                          content: [cell]
+                        })}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          );
+        }
+        return null;
+      })}
+    </div>
+  )}
+</Col>
+
+
+
+{/* 
+<Col>
+{blog && blog.body && (
+<div>{documentToReactComponents(blog.body)}</div>
+ )}
+</Col>  */}
+</Col>
+
+       
+
+       
+       
       </Row>
     </Container>
   );
