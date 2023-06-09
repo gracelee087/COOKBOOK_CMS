5ca2f3d42a4aba43f81fa9af818c6b3b48cdc7fa
Sohee Lee <gracelee.087@gmail.com>
2023-06-09 14:49:02 +0200

    breakfast menu navlink

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { client } from "../client";
 
-function Breakfast({ Breakfast }) {
+
+function Breakfast() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    // Fetch blogs from Contentful
+    client
+      .getEntries({
+        content_type: "blog",
+        "fields.Category": "Breakfast",
+      })
+      .then((response) => {
+        setBlogs(response.items);
+      })
+      .catch((error) => {
+        console.error("Error fetching breakfast content:", error);
+      });
+  }, []);
+
   return (
     <div>
-      breakfast
+      {blogs.map((blog) => (
+        <div key={blog.sys.id}>
+
+
+          <h2>{blog.fields.title}</h2>
+          {blog.fields.image && (
+            <img
+              src={blog.fields.image.fields.file.url}
+              alt={blog.fields.image.fields.title}
+            />
+          )}
+
+
+          {/* {blog.fields.body && (
+            <div>
+              {documentToReactComponents(blog.fields.body)}
+            </div>
+          )} */}
+
+
+
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Breakfast
+export default Breakfast;
